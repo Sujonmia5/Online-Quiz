80d40bec8941f7dd67cebb246ac4e7d3265b0e4f
Sujonmia5 <mdsujon673738@gmail.com>
2022-10-11 08:32:47 +0600

    Navbar added

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1>The online quiz maker that's easy to use, fun and fully customizable</h1>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/Layout/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../Components/Header/Header';
+import Navbar from '../Components/Navbar/Navbar';
+
+const Main = () => {
+    return (
+        <div>
+            <Navbar />
+        </div>
+    );
+};
+
+export default Main;
