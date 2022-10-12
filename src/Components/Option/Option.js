31c97335c327c00e6bbca3a834d493aad15c878e
Sujonmia5 <mdsujon673738@gmail.com>
2022-10-12 12:06:14 +0600

    navigate added

--- src/Components/Option/Option.js
+++ src/Components/Option/Option.js
@@ -3,8 +3,7 @@ import React from 'react';
 const Option = ({ option, btnHandler }) => {
 
     return (
-        <label onClick={() => btnHandler(option)} htmlFor={option} className='border rounded-lg p-3 flex justify-start items-center hover:bg-indigo-100' >
-            <input type="radio" className="radio radio-primary" />
+        <label onClick={() => btnHandler(option)} className='border rounded-lg p-3 flex justify-start items-center hover:bg-indigo-100' >
             <p className='ml-5 w-full' >{option}</p>
         </label>
     );
--- src/Components/QuizCart/QuizCart.js
+++ src/Components/QuizCart/QuizCart.js
@@ -1,7 +1,17 @@
-
 import { ArrowRightIcon } from '@heroicons/react/24/solid'
+import { useNavigate } from 'react-router-dom';
+
+
 export const QuizCart = ({ quiz }) => {
-    const { id, name, logo, total } = quiz
+    const { id, name, logo, total } = quiz;
+
+    const navigate = useNavigate()
+
+    const handleNavigate = () => {
+        navigate(`/quiz/${id}`)
+
+    }
+
     return (
         <div className="flex p-2 rounded-xl flex-col justify-between overflow-hidden text-left transition-shadow duration-200 bg-gray-900 text-white rounded shadow-xl group hover:shadow-2xl">
             <div className="p-4">
@@ -14,15 +24,15 @@ export const QuizCart = ({ quiz }) => {
                 <p className="text-sm leading-5">
                     Total Quiz: {total}
                 </p>
-                <a
-                    href={`/quiz/${id}`}
+                <button
+                    onClick={handleNavigate}
                     className="inline-flex items-center justify-center h-8 px-4 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-blue-600 hover:bg-blue-500 focus:shadow-outline focus:outline-none"
                     aria-label="Start quiz"
                     title="Start quiz"
                 >
                     Start quiz
                     <ArrowRightIcon className="h-6 w-5 ml-1 font-bold text-white" />
-                </a>
+                </button>
             </div>
             <div className="w-full h-1 ml-auto duration-300 origin-left transform scale-x-0 bg-deep-purple-accent-400 group-hover:scale-x-100" />
         </div>
